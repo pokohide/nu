dec8333c9d2211b44e1b81c59f43d1dc2e4c2488
SAKAI Hidenobu <nd2256345@iceb19.ice.nuie.nagoya-u.ac.jp>
2016-10-06 15:42:59 +0900

    16$B%S%C%H2C;;2sO)$N<B9T%U%!%$%k$H%F%9%H%U%!%$%k$rDI2C(B

---
+++ jikken/jikken2/adder16.v
@@ -0,0 +1,15 @@
+/*                 *
+ * adder16.v       *
+ * 16ビット加算回路*
+ *                 */
+
+module adder16(x, y, cin, sum, cout);
+  input [15:0] x, y;
+  input cin;
+  
+  output [15:0] sum;
+  output cout;
+
+  assign {cout, sum} = x + y + cin:
+endmodule
+
