09841d2fac4bf0e1fd28e89f577ad0bdcb4f2453
pokohide <hyde31415@gmail.com>
2016-10-13 13:59:14 +0900

    add jikken3 images

---
+++ jikken/jikken3/adder16s.v
@@ -0,0 +1,24 @@
+/*                              *
+ * adder16s.v                   *
+ * 順序回路版 16 ビット加算回路 *
+ *                              */
+
+module adder16s(clk, reset, x, y, cin, sum, cout);
+  input [15:0] x, y;
+  input clk, reset, cin;
+  output [15:0] sum;
+  output cout;
+
+  reg [15:0] r0, r1;
+
+  assign {cout, sum} = r0 + r1 + cin;
+
+  always @(posedge clk or negedge reset) begin
+    if(reset == 1'b0) begin
+      r0 <= 0; r1 <= 0;
+    end else begin
+      r0 <= x; r1 <= y;
+    end
+  end
+endmodule
+
---
+++ jikken/jikken3/test_adder16s.v
@@ -0,0 +1,33 @@
+/*                                            *
+ * test_adder16s.v                            *
+ * 順序回路版 16 ビット加算回路のテストベンチ *
+ *                                            */
+
+`timescale 1ns / 1ns  // シミュレーションの単位時間 / 精度
+`include "adder16s.v"
+
+module test;
+  reg reset, clk, cin;
+  reg [15:0] x, y;
+
+  wire [15:0] sum;
+  wire cout;
+
+  adder16s adder16sa(clk, reset, x, y, cin, sum, cout);
+
+  always begin
+    #5 clk = ~clk;
+  end
+
+  always begin
+    #8 x = x + 100;
+       y = y + 200;
+  end
+
+  initial begin
+    reset = 1; clk = 0; x = 0; y = 0; cin = 0;
+    #20 reset = 0;
+    #20 reset = 1;
+  end
+endmodule
+
